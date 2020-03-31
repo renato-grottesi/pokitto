5147148c15998ec07fefa7f6c975a85a39afa00b
renato_grottesi <renato.grottesi@gmail.com>
2020-03-31 13:50:22 +0200

    almost everything works now

--- Bitmap/main.cpp
+++ Bitmap/main.cpp
@@ -52,7 +52,7 @@ static void prepare(void)
 	SET_MASK_P2;
 }
 
-uint32_t __attribute__((section (".bss"))) __attribute__ ((aligned)) screenbuffer[220];
+uint32_t __attribute__((section (".bss"))) __attribute__ ((aligned)) screenbuffer[220*2]; // TODO: the *2 is to prevent buffer overflow
 
 static void colorscreen() {
 	// 32 bits of the register with the color:
@@ -108,20 +108,43 @@ const uint16_t* palettes[spriteCount] = {
 	sprite4_pal,
 };
 
-void drawBitmap(const uint16_t screen_y, const int16_t bmp_x, const int16_t bmp_y, const uint16_t* palette, const uint8_t* bitmap) {
+void drawBitmapPal16(const uint16_t screen_y, const int16_t bmp_x, const int16_t bmp_y, const uint16_t* palette, const uint8_t* bitmap) {
 	if(screen_y>=bmp_y && screen_y<bmp_y+bitmap[1]) {
 		uint16_t bmpy = screen_y-bmp_y;
 		for(uint8_t x = 0; x < bitmap[0]/2; x++) {
 			if(bmp_x+x*2 < 220) {
 				uint8_t twocolors = bitmap[2 + bmpy*(bitmap[0]/2) + x];
-				uint32_t col16_1 = palette[((twocolors&0xF0)>>4)&0xF];
-				uint32_t col16_2 = palette[twocolors&0x0F];
+				uint32_t col16_1 = palette[(twocolors>>4)&0xF];
+				uint32_t col16_2 = palette[(twocolors>>0)&0xF];
 				// Shift from a 565 16 bit to the 32 bit madness
 				// FEDCBA9876543210FEDCBA9876543210
 				// ------------RRRRRRGGGGGGBBBBBB--
 				// ----------------RRRRRGGGGGGBBBBB
-				if((((twocolors&0xF0)>>4)&0xF)!=1) screenbuffer[bmp_x+x*2+0] = ((col16_1&0xF800) << 4) + ((col16_1&0x07E0) << 3) + ((col16_1&0x001F) << 2);
-				if((twocolors&0x0F)!=1)screenbuffer[bmp_x+x*2+1] = ((col16_2&0xF800) << 4) + ((col16_2&0x07E0) << 3) + ((col16_2&0x001F) << 2);
+				if(((twocolors>>4)&0xF)!=1) screenbuffer[bmp_x+x*2+0] = ((col16_1&0xF800) << 4) + ((col16_1&0x07E0) << 3) + ((col16_1&0x001F) << 2);
+				if(((twocolors>>0)&0xF)!=1) screenbuffer[bmp_x+x*2+1] = ((col16_2&0xF800) << 4) + ((col16_2&0x07E0) << 3) + ((col16_2&0x001F) << 2);
+			}
+		}
+	}
+}
+
+void drawBitmapPal4(const uint16_t screen_y, const int16_t bmp_x, const int16_t bmp_y, const uint16_t* palette, const uint8_t* bitmap) {
+	if(screen_y>=bmp_y && screen_y<bmp_y+bitmap[1]) {
+		uint16_t bmpy = screen_y-bmp_y;
+		for(uint8_t x = 0; x < bitmap[0]/4; x++) {
+			if(bmp_x+x*4 < 220) {
+				uint8_t twocolors = bitmap[2 + bmpy*(bitmap[0]/4) + x];
+				uint32_t col16_1 = palette[(twocolors>>6)&0x3];
+				uint32_t col16_2 = palette[(twocolors>>4)&0x3];
+				uint32_t col16_3 = palette[(twocolors>>2)&0x3];
+				uint32_t col16_4 = palette[(twocolors>>0)&0x3];
+				// Shift from a 565 16 bit to the 32 bit madness
+				// FEDCBA9876543210FEDCBA9876543210
+				// ------------RRRRRRGGGGGGBBBBBB--
+				// ----------------RRRRRGGGGGGBBBBB
+				if(((twocolors>>6)&0xF)!=0) screenbuffer[bmp_x+x*4+0] = ((col16_1&0xF800) << 4) + ((col16_1&0x07E0) << 3) + ((col16_1&0x001F) << 2);
+				if(((twocolors>>4)&0xF)!=0) screenbuffer[bmp_x+x*4+1] = ((col16_2&0xF800) << 4) + ((col16_2&0x07E0) << 3) + ((col16_2&0x001F) << 2);
+				if(((twocolors>>2)&0xF)!=0) screenbuffer[bmp_x+x*4+2] = ((col16_3&0xF800) << 4) + ((col16_3&0x07E0) << 3) + ((col16_3&0x001F) << 2);
+				if(((twocolors>>0)&0xF)!=0) screenbuffer[bmp_x+x*4+3] = ((col16_4&0xF800) << 4) + ((col16_4&0x07E0) << 3) + ((col16_4&0x001F) << 2);
 			}
 		}
 	}
@@ -148,7 +171,7 @@ int main() {
 
 	// Game loop
 	while (mygame.isRunning()) {
-#if 1
+
 		if (mygame.update(true)) {
 			prepare();
 			for(uint8_t y = 0; y < 176; y++) {
@@ -159,61 +182,56 @@ int main() {
 					screenbuffer[x] = r + g + b;
 				}
 
-				drawBitmap(y, icon_x, icon_y, pokitto_icon_pal, pokitto_icon);
+				drawBitmapPal4(y, 0, 0, background_pal, background_bmp);
+
+				drawBitmapPal16(y, icon_x, icon_y, pokitto_icon_pal, pokitto_icon);
+
+				for (int i = 0; i < spriteCount; i++) {
+					drawBitmapPal4(y, mySprites[i].x, mySprites[i].y, palettes[i], sprite_bmp);
+				}
 
 				colorscreen();
 			}
-#else
-			if (mygame.update(false)) {
-				// Draw the background only in the first rounds.
-				mygame.display.setInvisibleColor(-1);
-				mygame.display.load565Palette(background_pal);
-				mygame.display.drawBitmap(0, 0, background_bmp);
-				// Move mySprites
-				for (int i = 0; i < spriteCount; i++) {
-					int16_t x = mySprites[i].x, y = mySprites[i].y;
-					int16_t w = mySprites[i].w, h = mySprites[i].h;
 
-					// Advance x and y.
-					x += mySprites[i].vx;
-					y += mySprites[i].vy;
+			// Move mySprites
+			for (int i = 0; i < spriteCount; i++) {
+				int16_t x = mySprites[i].x, y = mySprites[i].y;
+				int16_t w = mySprites[i].w, h = mySprites[i].h;
 
-					// Bounce from hidden edges
-					if (x < -spriteW) {
-						x = -spriteW;
-						mySprites[i].vx = speed;
-					} else if (x >= mygame.display.getWidth() - w + spriteW) {
-						x = mygame.display.getWidth() - 1 - w + spriteW;
-						mySprites[i].vx = -speed;
-					}
-					if (y < -spriteH) {
-						y = -spriteH;
-						mySprites[i].vy = speed;
-					} else if (y >= mygame.display.getHeight() - h + spriteH) {
-						y = mygame.display.getHeight() - 1 - h + spriteH;
-						mySprites[i].vy = -speed;
-					}
+				// Advance x and y.
+				x += mySprites[i].vx;
+				y += mySprites[i].vy;
 
-					mygame.display.setInvisibleColor(0);
-					mygame.display.load565Palette(palettes[i]);
-					mygame.display.drawBitmap(x, y, sprite_bmp);
-
-					//
-					mySprites[i].x = x;
-					mySprites[i].y = y;
+				// Bounce from hidden edges
+				if (x < -spriteW) {
+					x = -spriteW;
+					mySprites[i].vx = speed;
+				} else if (x >= mygame.display.getWidth() - w + spriteW) {
+					x = mygame.display.getWidth() - 1 - w + spriteW;
+					mySprites[i].vx = -speed;
 				}
-#endif
-#if 1
-				if (mygame.buttons.rightBtn()) { icon_x++; }
-				if (mygame.buttons.leftBtn()) { icon_x--; }
-				if (icon_x < 1) { icon_x = 1; }
-				if (icon_x > 64) { icon_x = 64; }
-				if (mygame.buttons.downBtn()) { icon_y++; }
-				if (mygame.buttons.upBtn()) { icon_y--; }
-				if (icon_y < 1) { icon_y = 1; }
-				if (icon_y > 64) { icon_y = 64; }
-#endif
+				if (y < -spriteH) {
+					y = -spriteH;
+					mySprites[i].vy = speed;
+				} else if (y >= mygame.display.getHeight() - h + spriteH) {
+					y = mygame.display.getHeight() - 1 - h + spriteH;
+					mySprites[i].vy = -speed;
+				}
+
+				//
+				mySprites[i].x = x;
+				mySprites[i].y = y;
 			}
+
+			if (mygame.buttons.rightBtn()) { icon_x++; }
+			if (mygame.buttons.leftBtn()) { icon_x--; }
+			if (icon_x < 1) { icon_x = 1; }
+			if (icon_x > 64) { icon_x = 64; }
+			if (mygame.buttons.downBtn()) { icon_y++; }
+			if (mygame.buttons.upBtn()) { icon_y--; }
+			if (icon_y < 1) { icon_y = 1; }
+			if (icon_y > 64) { icon_y = 64; }
 		}
-		return 0;
 	}
+	return 0;
+}
